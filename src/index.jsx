e530a28ba5bbeb55ce62ce8a9f4e5688224fa9fb
Viktoria <doduchenko29@gmail.com>
2021-07-16 11:59:06 +0300

    add

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './index.js';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Game />, rootElement);
